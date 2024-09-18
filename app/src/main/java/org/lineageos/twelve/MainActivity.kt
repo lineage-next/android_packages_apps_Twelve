c615c8acbac8ab40adb526689659c892a4c2d9f2
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-09-18 20:37:44 +0200

    Twelve: Open app by clicking notification

--- app/src/main/java/org/lineageos/twelve/MainActivity.kt
+++ app/src/main/java/org/lineageos/twelve/MainActivity.kt
@@ -8,12 +8,35 @@ package org.lineageos.twelve
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import kotlin.reflect.cast
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    // NavController
+    private val navHostFragment by lazy {
+        NavHostFragment::class.cast(
+            supportFragmentManager.findFragmentById(R.id.navHostFragment)
+        )
+    }
+    private val navController by lazy { navHostFragment.navController }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Enable edge-to-edge
         enableEdgeToEdge()
+
+        // Handle now playing
+        if (intent.getBooleanExtra(EXTRA_OPEN_NOW_PLAYING, false)) {
+            navController.navigate(R.id.fragment_now_playing)
+        }
+    }
+
+    companion object {
+        /**
+         * Open now playing fragment.
+         * Type: [Boolean]
+         */
+        const val EXTRA_OPEN_NOW_PLAYING = "extra_now_playing"
     }
 }
--- app/src/main/java/org/lineageos/twelve/services/PlaybackService.kt
+++ app/src/main/java/org/lineageos/twelve/services/PlaybackService.kt
@@ -5,12 +5,14 @@
 
 package org.lineageos.twelve.services
 
+import android.app.PendingIntent
 import android.content.Intent
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession
+import org.lineageos.twelve.MainActivity
 
 class PlaybackService : MediaLibraryService() {
     private var mediaLibrarySession: MediaLibrarySession? = null
@@ -34,7 +36,9 @@ class PlaybackService : MediaLibraryService() {
 
         mediaLibrarySession = MediaLibrarySession.Builder(
             this, exoPlayer, mediaLibrarySessionCallback
-        ).build()
+        )
+            .setSessionActivity(getSingleTopActivity())
+            .build()
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
@@ -54,4 +58,13 @@ class PlaybackService : MediaLibraryService() {
     }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo) = mediaLibrarySession
+
+    private fun getSingleTopActivity() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).apply {
+            putExtra(MainActivity.EXTRA_OPEN_NOW_PLAYING, true)
+        },
+        PendingIntent.FLAG_IMMUTABLE
+    )
 }
